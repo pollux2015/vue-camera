2e95f28a01470f4db41c80470f2b6d2e4d262663
pollux <pollux@polluxMac.local>
2018-01-10 17:06:14 +0800

    add doc

--- config/index.js
+++ config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../doc/index.html'),
+    index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../doc'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
 
